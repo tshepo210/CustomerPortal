2e7c1f1ca5e5d36b6572a08f5d6a271a921a7e09
tshepo210 <tshepo.thulo@outlook.com>
2026-06-10 16:50:53 +0200

    Devops (#4)
    * devops init
    
    * sonarqube vars
    
    * sonar fix
    
    * added org
    
    * check results
    
    * removed register
    
    * user management

---
+++ CustomerPortal/Models/UserListViewModel.cs
@@ -0,0 +1,11 @@
+namespace CustomerPortal.Models
+{
+    public class UserListViewModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Roles { get; set; } = string.Empty;
+    }
+}
